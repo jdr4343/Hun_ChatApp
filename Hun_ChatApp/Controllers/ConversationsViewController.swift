05f67ee5e6db80666e2575b72148f20aee9d3bfe
jdr4343 <jdr4343@naver.com>
2021-08-03 02:27:46 +0900

    Update ConversationsViewController.swift

--- Hun_ChatApp/Controllers/ConversationsViewController.swift
+++ Hun_ChatApp/Controllers/ConversationsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
@@ -17,15 +18,20 @@ class ConversationsViewController: UIViewController {
     //뷰가 표시되지 않도록 재정의
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-            let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
-        
-        if !isLoggedIn {
-            let vc = LoginViewController()
-            let nav = UINavigationController(rootViewController: vc)
-            //로그인한 상태가 아니라면 로그인 페이지 등록을 제출하지ㅣ 않고 닫을수 있게 아래로 스와이프 하여 해제 할수 있도록 구현
-            nav.modalPresentationStyle = .fullScreen
-            present(nav, animated: false)
+
+        validateAute()
+     
+        }
+    //회원가입을 한사람이 앱을 열때 로그인을 다시 표시 하지 않게 하기 위해서 앱이 시작될 때 인스턴스화 되는 ConversationsViewController로 이동 해서 코드를 다시 작성하겠습니다.
+        //인증 상태 확인, 검증
+        private func validateAute() {
+            // firebase에서 인증이 되었는지 확인 / 현재 사용자가 nil이라면 LoginViewController를 표시할것입니다. 
+            if FirebaseAuth.Auth.auth().currentUser == nil {
+                let vc = LoginViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                //로그인한 상태가 아니라면 로그인 페이지 등록을 제출하지ㅣ 않고 닫을수 있게 아래로 스와이프 하여 해제 할수 있도록 구현
+                nav.modalPresentationStyle = .fullScreen
+                present(nav, animated: false)
         }
     }
 
