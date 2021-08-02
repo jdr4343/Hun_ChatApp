49f10ad53128b769a4c35cc6b8bee32a8eeda785
jdr4343 <jdr4343@naver.com>
2021-08-03 02:27:42 +0900

    Create DatabaseManager.swift

---
+++ Hun_ChatApp/Resources/DatabaseManager.swift
@@ -0,0 +1,11 @@
+//
+//  DatabaseManager.swift
+//  Hun_ChatApp
+//
+//  Created by 신지훈 on 2021/08/03.
+//
+
+import Foundation
+import FirebaseDatabase
+//데이터베이스를 읽고 쓰는 객체를 만들어 데이터베이스를 읽고 쓸 수 있도록 하겠습니다.각 뷰에서 만들어도 되지만 중복코드가 있을수 있기 때문에 전용 파일을 만들겠습니다.
+
